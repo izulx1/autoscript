2246bec01180b4382be5b80b4b0f49ae956133d4
izulx1 <166265491+izulx1@users.noreply.github.com>
2024-05-06 16:28:34 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -10,3 +10,5 @@ wget -q -O /etc/systemd/system/vmess.service "${GITHUB_CMD}service/vmess.service
 wget -q -O /etc/systemd/system/vless.service "${GITHUB_CMD}service/vless.service"
 wget -q -O /etc/systemd/system/trojan.service "${GITHUB_CMD}service/trojan.service"
 wget -q -O /etc/systemd/system/ss.service "${GITHUB_CMD}service/ss.service"
+
+rm -f ins-xray.sh >/dev/null
